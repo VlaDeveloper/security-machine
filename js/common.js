5bec29a6745ffe5348adbfa0594ebb0afae885cf
VlaDeveloper <43091115+VlaDeveloper@users.noreply.github.com>
2023-11-16 17:37:26 +0500

    Changed color scheme and stuff
    White scheme, popup, animated top line

--- js/common.js
+++ js/common.js
@@ -1,13 +1,37 @@
 // $('body').hide()
+$(function () {
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 50){
+				$('.top-line').addClass('active');
+			}else{
+				$('.top-line').removeClass('active');
+			}
+	});
 
-$('.sec-inner').equalHeight();
+	$(".sec-carousel").owlCarousel({
+		items: 1,
+		dots: false,
+		nav: true,
+		navText: ['<img class="img-svg" src="images/angle-left-solid.svg" alt="Left">','<img class="img-svg" src="images/angle-right-solid.svg" alt="Right">']
+	});
+	
+	$("img.img-svg").each(function(){var t=$(this),r=t.attr("id"),a=t.attr("class"),e=t.attr("src");$.get(e,function(e){var i=$(e).find("svg");void 0!==r&&(i=i.attr("id",r)),void 0!==a&&(i=i.attr("class",a+" replaced-svg")),!(i=i.removeAttr("xmlns:a")).attr("viewBox")&&i.attr("height")&&i.attr("width")&&i.attr("viewBox","0 0 "+i.attr("height")+" "+i.attr("width")),t.replaceWith(i)},"xml")});
 
-$(".sec-carousel").owlCarousel({
-	items: 1,
-	dots: false,
-	nav: true,
-	navText: ['<img class="img-svg" src="images/angle-left-solid.svg" alt="Left">','<img class="img-svg" src="images/angle-right-solid.svg" alt="Right">']
-});
+	$('.sec-carousel-item').each(function(e){
+		$(this).find('.sec-carousel-link').attr('href', '#sec-carousel-popup-' + e);
+		$(this).find('.sec-carousel-popup').attr('id', 'sec-carousel-popup-' + e);
+	});
+	$('.sec-carousel-link').magnificPopup();
 
-$("img.img-svg").each(function(){var t=$(this),r=t.attr("id"),a=t.attr("class"),e=t.attr("src");$.get(e,function(e){var i=$(e).find("svg");void 0!==r&&(i=i.attr("id",r)),void 0!==a&&(i=i.attr("class",a+" replaced-svg")),!(i=i.removeAttr("xmlns:a")).attr("viewBox")&&i.attr("height")&&i.attr("width")&&i.attr("viewBox","0 0 "+i.attr("height")+" "+i.attr("width")),t.replaceWith(i)},"xml")});
+	function makeEqual() {
+		$('.sec-inner').height('auto').equalHeight();
+	}
 
+	makeEqual()
+	
+	$(window).resize(function () {
+		makeEqual()
+	})
+
+})
